78ee162bdaccea137384e4d44547c44617ca7c45
KiRist <37296174+KiRist-code@users.noreply.github.com>
2021-02-08 05:23:01 -0800

    Delete 2. add directory

--- 2. add/addExample.s
+++
@@ -1,7 +0,0 @@
-.global _start
-
-_start: mvn	r0, #2
-	add	r0, #1
-
-	mov	r7, #0
-	svc	0
--- 2. add/addexample1.s
+++
@@ -1,9 +0,0 @@
-.global _start
-.align 2
-
-_start: movn	w0, #2
-	add	w0, w0, #1
-
-	mov	x16, #1
-	svc	#0x80
-
