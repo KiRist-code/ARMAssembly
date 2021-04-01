29774e7352aeb92e3eaf4db7fa2eef77edbbecc8
KiRist <37296174+KiRist-code@users.noreply.github.com>
2021-04-01 10:30:56 +0900

    Add files via upload

---
+++ chapter3/testasm/testasm/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  chapter3
+//
+//  Created by 김민준 on 2021/03/08.
+//
+
+#include <stdio.h>
+
+extern void start( void );
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    start();
+    return 0;
+}
